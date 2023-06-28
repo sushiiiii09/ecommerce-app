9d807f20863ed631a833bbf220a7325cc757cbaa
sushiiiii09 <sshreechoudhury@gmail.com>
2023-06-28 22:47:18 +0530

    Shop section Completed

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>
-      Hello from cart
+    <div className='cart'>
+      <h1>Your Cart Items</h1>
     </div>
   )
 }
--- src/pages/shop/product.jsx
+++ src/pages/shop/product.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/shop-context';
 
 const Product = (props) => {
     const {id,productName,price,productImage}=props.data;
+    const {addToCart,cartItems}=useContext(ShopContext);
+  const cartItemAmount = cartItems[id]
   return (
     <div className="product">
         
-        <img src={productImage}/>
+        <img src={productImage} alt='some product'/>
         <div className="desciption">
             <p>
                 <b>{productName}</b>
@@ -13,13 +16,11 @@ const Product = (props) => {
             <p>
             ₹ {price}
             </p>            
-            <button className='addToCartBttn'> Add to cart</button>
-
-
+            <button className='addToCartBttn' 
+            
+            onClick={()=>addToCart(id)}> Add to cart {cartItemAmount>0 && <>({cartItemAmount})</>}</button>
         </div>
-    
-    
-    
+     
     
     </div>
   )
